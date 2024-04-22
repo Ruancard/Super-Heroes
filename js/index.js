10a041211c302edce59b14d0b5ef6558b64f8a89
Ruancard <103271130+Ruancard@users.noreply.github.com>
2024-04-22 14:55:26 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,27 +2,32 @@ const images = document.querySelectorAll('.image')
 const texts = document.querySelectorAll('.text')
 const icons = document.querySelectorAll('.hero_icon')
 
-
-const imgObserver = new IntersectionObserver((entries) =>{
-     entries.forEach( (entry) => {
-          if(entry.isIntersecting) {
-               entry.target.classList.add('show_img')
-          } else {
-               entry.target.classList.remove('show_img')
-          }
-     })
-})
 const textObserver = new IntersectionObserver((entries) =>{
      entries.forEach( (entry) => {
           if(entry.isIntersecting) {
                entry.target.classList.add('show_text')
+
+               images.forEach( (image) => {
+                    if(image.children[0].id == `img_${entry.target.id}`){
+                         image.classList.add('show_img')
+                    }
+               })
+
                icons.forEach( (icon) => {
                     if(icon.classList[1] == entry.target.id){
                          icon.classList.add('shiny')
                     }
                })
+               
           } else {
                entry.target.classList.remove('show_text')
+
+               images.forEach( (image) => {
+                    if(image.children[0].id == `img_${entry.target.id}`){
+                         image.classList.remove('show_img')
+                    }
+               })
+
                icons.forEach( (icon) => {
                     if(icon.classList[1] == entry.target.id){
                          icon.classList.remove('shiny')
@@ -31,6 +36,4 @@ const textObserver = new IntersectionObserver((entries) =>{
           }
      })
 })
-
-images.forEach((image) => imgObserver.observe(image))
 texts.forEach((text) => textObserver.observe(text))
